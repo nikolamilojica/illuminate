38dace2e4beda01ca924f355528a3587aaa3a448
Nikola Milojica <nikola.milojica@protonmail.com>
2022-04-26 22:16:45 +0200

    Remove __init__ from manager interface class

--- illuminate/discrete/manager/interface.py
+++ illuminate/discrete/manager/interface.py
@@ -1,7 +1,5 @@
 class Interface(object):
     """Interface for manager class"""
-    def __init__(self, name, path):
-        raise NotImplementedError
 
     def create_project(self):
         """Create project directory and populates it with needed files"""
