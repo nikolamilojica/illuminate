c4e59769a849cca5d8fb5c0b927a4de7ba1af6a7
Nikola Milojica <nikola.milojica@protonmail.com>
2022-06-10 20:26:45 +0200

    Remove wrong doc string

--- illuminate/adapter/adapter.py
+++ illuminate/adapter/adapter.py
@@ -3,7 +3,7 @@ from illuminate.exceptions.manager import BasicManagerException
 
 
 class Adapter(Interface):
-    """Interface for adapter class"""
+
     def adapt(self, item, *args, **kwargs):
         """Perform transformation upon item"""
         raise BasicManagerException
