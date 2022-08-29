1276d1b55736fc4b7ddcca999b104f39a7c3e12d
Nikola Milojica <nikola.milojica@protonmail.com>
2022-08-29 18:53:44 +0200

    Add SQLExporter.export logging

--- illuminate/exporter/sql.py
+++ illuminate/exporter/sql.py
@@ -1,12 +1,19 @@
+from loguru import logger
+
 from illuminate.exporter.exporter import Exporter
 
 
 class SQLExporter(Exporter):
     """SQLExporter class, responsible for writing to destination"""
+
     def __init__(self, model):
         self.model = model
 
     def export(self, session, *args, **kwargs):
         """Load to destination"""
         session.add(self.model)
-        session.commit()
+        session.commit()  # TODO: try/catch
+        logger.info(f'{self}.export(session="{session}")')
+
+    def __repr__(self):
+        return f"SQLExporter(model={self.model})"
