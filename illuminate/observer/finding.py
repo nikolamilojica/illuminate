e03f86fafd43ba890e53c43d7cdf6e58afe951e7
Nikola Milojica <nikola.milojica@protonmail.com>
2022-09-20 21:36:47 +0200

    Clean illuminate/observer/finding.py from code smells

--- illuminate/observer/finding.py
+++ illuminate/observer/finding.py
@@ -3,6 +3,3 @@ from illuminate.discrete.observer.finding import Interface
 
 class Finding(Interface):
     """Finding class, responsible for holding data"""
-
-    def __init__(self):
-        pass
