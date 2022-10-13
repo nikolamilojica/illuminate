68059250436035e3caa847f3f1ca68819f702f27
Nikola Milojica <47486247+nikolamilojica@users.noreply.github.com>
2022-10-14 00:57:46 +0200

    feat/adapter-priority (#37)
    * Introduce priority attribute
    * Add priority attribute with short explanation
    * Sort Adapters by priority score

--- illuminate/adapter/adapter.py
+++ illuminate/adapter/adapter.py
@@ -5,6 +5,9 @@ from illuminate.interface.adapter import IAdapter
 class Adapter(IAdapter):
     """Adapter class, responsible for producing exporters"""
 
+    priority = None
+    subscribers = None
+
     async def adapt(self, finding, *args, **kwargs):
         """Transform finding and produce exporters"""
         raise BasicAdapterException
