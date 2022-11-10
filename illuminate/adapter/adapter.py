5ced3232175cef3be07af56ff887cd4b35697d07
Nikola Milojica <nikola.milojica@protonmail.com>
2022-11-10 16:48:00 +0100

    Add typing hints for Adapter class

--- illuminate/adapter/adapter.py
+++ illuminate/adapter/adapter.py
@@ -1,14 +1,24 @@
+from __future__ import annotations
+
+from collections.abc import AsyncGenerator
+from typing import Type, Union
+
 from illuminate.exceptions.adapter import BasicAdapterException
+from illuminate.exporter.exporter import Exporter
 from illuminate.interface.adapter import IAdapter
+from illuminate.observation.observation import Observation
+from illuminate.observer.finding import Finding
 
 
 class Adapter(IAdapter):
     """Adapter class, responsible for producing exporters"""
 
-    priority = None
-    subscribers = None
+    priority: int
+    subscribers: tuple[Type[Finding]]
 
-    async def adapt(self, finding, *args, **kwargs):
+    async def adapt(
+        self, finding: Finding, *args, **kwargs
+    ) -> AsyncGenerator[Union[Exporter, Observation], None]:
         """Transform finding and produce exporters"""
         raise BasicAdapterException(
             "Method adapt must be implemented in child class"
