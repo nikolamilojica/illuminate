6277f577442c8b2781d94ce1db9ddfb853863f98
Nikola Milojica <nikola.milojica@protonmail.com>
2022-11-10 16:52:55 +0100

    Add typing hints for HTTPObservation class

--- illuminate/observation/http.py
+++ illuminate/observation/http.py
@@ -1,28 +1,49 @@
+from __future__ import annotations
+
+from collections.abc import AsyncGenerator
+from typing import Callable, Optional, Union
+
 from loguru import logger
 from tornado import httpclient
 from tornado.httpclient import HTTPClientError
+from tornado.httpclient import HTTPResponse
 
 from illuminate.exceptions.observation import BasicObservationException
+from illuminate.exporter.exporter import Exporter
 from illuminate.observation.observation import Observation
+from illuminate.observer.finding import Finding
 
 
 class HTTPObservation(Observation):
     """HTTPObservation class, responsible for reading source with callback"""
 
-    def __init__(self, url, /, allowed, callback, *args, **kwargs):
+    def __init__(
+        self,
+        url: str,
+        /,
+        allowed: Union[list[str], tuple[str]],
+        callback: Callable[
+            [HTTPResponse, tuple, dict],
+            AsyncGenerator[Union[Exporter, Finding, Observation], None],
+        ],
+        *args,
+        **kwargs,
+    ):
         self.url = url
         self._allowed = allowed
         self._callback = callback
         self.configuration = kwargs
 
     @property
-    def allowed(self):
+    def allowed(self) -> bool:
         for allowed in self._allowed:
             if self.url.startswith(allowed):
                 return True
         return False
 
-    async def observe(self, *args, **kwargs):
+    async def observe(
+        self, *args, **kwargs
+    ) -> Optional[AsyncGenerator[Union[Exporter, Finding, Observation], None]]:
         """Read source and use observer's callback function against response"""
         try:
             response = await httpclient.AsyncHTTPClient().fetch(
@@ -32,7 +53,7 @@ class HTTPObservation(Observation):
             return self._callback(response, *args, **kwargs)
         except HTTPClientError as exception:
             logger.warning(f"{self}.observe() -> {exception}")
-            return
+            return None
         except Exception as exception:
             logger.critical(f"{self}.observe() -> {exception}")
             raise BasicObservationException
