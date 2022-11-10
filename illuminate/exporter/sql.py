9d587dd2dd52ef790b236746a56ef801568c234a
Nikola Milojica <nikola.milojica@protonmail.com>
2022-11-10 16:50:26 +0100

    Add typing hints for SQLExporter class

--- illuminate/exporter/sql.py
+++ illuminate/exporter/sql.py
@@ -1,24 +1,34 @@
+from __future__ import annotations
+
+from typing import Type, TypeVar
+
 from loguru import logger
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from illuminate.exceptions.exporter import BasicExporterException
 from illuminate.exporter.exporter import Exporter
+
+M = TypeVar("M")
 
 
 class SQLExporter(Exporter):
     """SQLExporter class, responsible for writing to destination"""
 
-    def __init__(self, model):
-        self.model = model
-        self.name = None
-        self.type = None
+    name: str
+    type: str
 
-    async def export(self, session, *args, **kwargs):
+    def __init__(self, model: M):
+        self.model = model
+
+    async def export(
+        self, session: Type[AsyncSession], *args, **kwargs
+    ) -> None:
         """Load to destination"""
-        async with session() as session:
-            async with session.begin():
-                session.add(self.model)
+        async with session() as session:  # type: ignore
+            async with session.begin():  # type: ignore
+                session.add(self.model)  # type: ignore
                 try:
-                    await session.commit()
+                    await session.commit()  # type: ignore
                 except Exception as exception:
                     logger.critical(
                         f'{self}.export(session="{session}") -> {exception}'
