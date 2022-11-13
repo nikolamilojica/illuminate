0d457cefffdbd1d6b873edc5677d8a1b16e6e761
Nikola Milojica <nikola.milojica@protonmail.com>
2022-11-13 21:54:50 +0100

    Expand doc strings for SQLExporter class

--- illuminate/exporter/sql.py
+++ illuminate/exporter/sql.py
@@ -12,18 +12,48 @@ M = TypeVar("M")
 
 
 class SQLExporter(Exporter):
-    """SQLExporter class, responsible for writing to destination"""
+    """
+    SQLExporter class, writes data to SQL database asynchronously. Inherits
+    Exporter class and implements export method.
+
+    Each SQLExporter object is responsible for a single transaction with a
+    single database. Attributes name and type are used to acquire database
+    session object from Manager's sessions attribute.
+
+    Supported dialects:
+        - Mysql
+        - Postgres
+    """
 
     name: str
+    """
+    SQL database name selector used to get database session object from
+    Manager.sessions attribute.
+    """
     type: str
+    """
+    SQL database type selector used to get database session object from
+    Manager.sessions attribute.
+    """
 
     def __init__(self, model: M):
+        """
+        SQLExporter's __init__ method.
+
+        :param model: SQLAlchemy model object
+        """
         self.model = model
 
     async def export(
         self, session: Type[AsyncSession], *args, **kwargs
     ) -> None:
-        """Load to destination"""
+        """
+        Writes data to SQL database asynchronously.
+
+        :param session: AsyncSession object
+        :return: None
+        :raises BasicExporterException:
+        """
         async with session() as session:  # type: ignore
             async with session.begin():  # type: ignore
                 session.add(self.model)  # type: ignore
@@ -37,4 +67,9 @@ class SQLExporter(Exporter):
         logger.success(f'{self}.export(session="{session}")')
 
     def __repr__(self):
+        """
+        SQLExporter's __repr__ method.
+
+        :return: String representation of an instance
+        """
         return f"SQLExporter(model={self.model})"
