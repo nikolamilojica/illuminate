fe149b37efa5fc37a01df7c5570fe336801aff8d
Nikola Milojica <nikola.milojica@protonmail.com>
2023-03-06 01:33:00 +0100

    Add SplashObservation class

--- illuminate/observation/http.py
+++ illuminate/observation/http.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+import urllib.parse
 from collections.abc import AsyncGenerator
+from copy import copy
 from typing import Callable, Optional, Union
 
 from loguru import logger
@@ -100,3 +102,75 @@ class HTTPObservation(Observation):
         :return: String representation of an instance
         """
         return f'HTTPObservation("{self.url}",callback="{self._callback}")'
+
+
+class SplashObservation(HTTPObservation):
+    """
+    SplashObservation class, reads data from HTTP server asynchronously.
+    Inherits HTTPObservation class and implements observe method.
+
+    Constructor's kwargs are used to create Splash service URL. For full list
+    of parameters visit https://splash.readthedocs.io/en/stable/api.html.
+
+    Note: URL is passed as positional argument. It will be used as param
+    in Splash service URL.
+    """
+
+    def __hash__(self) -> int:
+        """
+        SplashObservation object hash value.
+
+        :return: int
+        """
+        return hash(self.service)
+
+    @property
+    def service(self):
+        """
+        Constructs URL of Splash service
+
+        :return: Splash URL string
+        """
+        parameters = copy(self.configuration)
+        host = parameters.pop("host")
+        port = parameters.pop("port")
+        protocol = parameters.pop("protocol")
+        parameters["url"] = self.url
+        render = parameters.pop("render")
+        return (
+            f"{protocol}://{host}:{port}/render.{render}?"
+            f"{urllib.parse.urlencode(parameters)}"
+        )
+
+    async def observe(
+        self, configuration: dict, *args, **kwargs
+    ) -> Optional[AsyncGenerator[Union[Exporter, Finding, Observation], None]]:
+        """
+        Requests data from HTTP server and renders response with Splash, passes
+        response object to a callback and returns async Exporter, Finding, and
+        Observation object generator if request is successful.
+
+        :param configuration: HTTP configuration dict from settings.py
+        :return: Async Exporter, Finding, and Observation object generator or
+        None
+        """
+        try:
+            response = await httpclient.AsyncHTTPClient().fetch(
+                self.service, **configuration
+            )
+            logger.info(f"{self}.observe() -> {response}")
+            return self._callback(response, *args, **kwargs)
+        except HTTPClientError as exception:
+            logger.warning(f"{self}.observe() -> {exception}")
+            return None
+        except Exception as exception:
+            logger.critical(f"{self}.observe() -> {exception}")
+            return None
+
+    def __repr__(self):
+        """
+        SplashObservation's __repr__ method.
+
+        :return: String representation of an instance
+        """
+        return f'SplashObservation("{self.url}",callback="{self._callback}")'
