24b54c3046e25bda3f40f44a43281b8e7440d29a
Nikola Milojica <nikola.milojica@protonmail.com>
2023-03-06 01:43:43 +0100

    Refactor tests/test_observation_http.py

--- tests/test_observation_http.py
+++ tests/test_observation_http.py
@@ -1,49 +1,14 @@
-import asyncio
-import io
 import json
 
 import pytest
-from tornado.httpclient import HTTPClientError
-from tornado.httpclient import HTTPRequest
-from tornado.httpclient import HTTPResponse
 
 from illuminate import __version__
-from illuminate.exceptions import BasicObservationException
 from illuminate.observation import HTTPObservation
 
 
-class TestSQLExporterClass:
+class TestHTTPObservationClass:
 
-    function = "tornado.httpclient.AsyncHTTPClient.fetch"
     url = "https//:example.com"
-
-    @staticmethod
-    @pytest.fixture(scope="function")
-    def async_http_response_ok(mocker):
-        """
-        Patch fetch function to return predefined response object.
-        """
-        future = asyncio.Future()
-        request = HTTPRequest(TestSQLExporterClass.url)
-        response = HTTPResponse(
-            request, 200, None, io.BytesIO(b'{"data": true}')
-        )
-        future.set_result(response)
-        mocker.patch(TestSQLExporterClass.function, return_value=future)
-
-    @staticmethod
-    @pytest.fixture(scope="function")
-    def async_http_response_not_ok(mocker):
-        """
-        Patch fetch function to raise HTTPClientError.
-        """
-
-        def _side_effect(*args, **kwargs):
-            raise HTTPClientError(509, "Timeout", None)
-
-        future = asyncio.Future()
-        future.set_result(_side_effect)
-        mocker.patch(TestSQLExporterClass.function, side_effect=_side_effect)
 
     def test_hash_implementation_with_same_configuration(self):
         """
@@ -113,7 +78,7 @@ class TestSQLExporterClass:
             **config_1,
         )
         observation_2 = HTTPObservation(
-            "https://example.com/apiv1",
+            "https://example.com/api/v1",
             allowed=(self.url,),
             callback=int,
             **config_2,
@@ -123,7 +88,7 @@ class TestSQLExporterClass:
     def test_not_allowed(self):
         """
         Given: Value of url is not contained in allowed collection and
-        observation is initialized
+        HTTPObservations is initialized
         When: Accessing allowed property
         Expected: Returns False
         """
@@ -135,7 +100,7 @@ class TestSQLExporterClass:
     def test_allowed(self):
         """
         Given: Value of url is contained in allowed collection and
-        observation is initialized
+        HTTPObservations is initialized
         When: Accessing allowed property
         Expected: Returns True
         """
@@ -145,25 +110,24 @@ class TestSQLExporterClass:
         assert observation.allowed
 
     @pytest.mark.asyncio
-    @pytest.mark.xfail(raises=BasicObservationException)
     async def test_observe_callback_unsuccessfully(
         self, async_http_response_ok
     ):
         """
-        Given: Observation is initialized with faulty callback function
+        Given: HTTPObservations is initialized with faulty callback function
         When: Instance calls observe function
         Expected: BasicObservationException is raised
         """
-        with pytest.raises(BasicObservationException):
-            observation = HTTPObservation(
-                self.url, allowed=(self.url,), callback=int
-            )
-            await observation.observe()
+        observation = HTTPObservation(
+            self.url, allowed=(self.url,), callback=int
+        )
+        result = await observation.observe()
+        assert not result
 
     @pytest.mark.asyncio
     async def test_observe_callback_successfully(self, async_http_response_ok):
         """
-        Given: Observation is initialized with proper callback function
+        Given: HTTPObservations is initialized with proper callback function
         When: Instance calls observe function
         Expected: Callback returns object without exception raised
         """
@@ -182,7 +146,7 @@ class TestSQLExporterClass:
         self, async_http_response_not_ok
     ):
         """
-        Given: Observation is initialized with proper callback function
+        Given: HTTPObservations is initialized with proper callback function
         When: Instance calls observe function but HTTPClientError is raised
         Expected: Function observe returns None
         """
