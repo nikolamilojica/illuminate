7460558cb5312268b1a23b468f6b654b8f0bbab0
Nikola Milojica <nikola.milojica@protonmail.com>
2023-07-06 00:12:58 +0200

    Add tests to check if BasicManagerException text is communicated through interface when user tries to run Alembic operations on unsupported database

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,6 +7,9 @@ from tests.unit import Test
 
 
 class TestCLI(Test):
+
+    influxdb_url = "influxdb://son:son@localhost/example"
+
     def test_manage_project_setup_unsuccessfully(self):
         """
         Given: There is already a folder with same name
@@ -38,6 +41,34 @@ class TestCLI(Test):
                 )
                 assert "Project structure created" in result.output
 
+    def test_manage_db_revision_exception(self):
+        """
+        Given: Current directory is a project directory
+        When: Running 'illuminate manage db revision' referencing wrong
+        database type with selector option
+        Expected: BasicManagerException is raised
+        """
+        with self.path() as path:
+            name = "example"
+            runner = CliRunner()
+            with runner.isolated_filesystem(temp_dir=path) as tmp:
+                runner.invoke(cli, ["manage", "project", "setup", name, "."])
+                result = runner.invoke(
+                    cli,
+                    [
+                        "manage",
+                        "db",
+                        "revision",
+                        "--selector",
+                        "measurements",
+                        tmp,
+                    ],
+                )
+                assert (
+                    "Command revision can only be performed on SQL "
+                    "database, measurements is not supported SQL database"
+                ) in result.exception.__str__()
+
     def test_manage_db_revision_unsuccessfully(self):
         """
         Given: Current directory is not a project directory
@@ -65,6 +96,37 @@ class TestCLI(Test):
                     cli, ["manage", "db", "revision", "--url", self.url, tmp]
                 )
                 assert "Revision created" in result.output
+
+    def test_manage_db_upgrade_exception(self):
+        """
+        Given: Current directory is a project directory
+        When: Running 'illuminate manage db upgrade' referencing wrong
+        database type with selector option
+        Expected: BasicManagerException is raised
+        """
+        with self.path() as path:
+            name = "example"
+            runner = CliRunner()
+            with runner.isolated_filesystem(temp_dir=path) as tmp:
+                runner.invoke(cli, ["manage", "project", "setup", name, "."])
+                runner.invoke(
+                    cli, ["manage", "db", "revision", "--url", self.url, tmp]
+                )
+                result = runner.invoke(
+                    cli,
+                    [
+                        "manage",
+                        "db",
+                        "upgrade",
+                        "--selector",
+                        "measurements",
+                        tmp,
+                    ],
+                )
+                assert (
+                    "Command upgrade can only be performed on SQL "
+                    "database, measurements is not supported SQL database"
+                ) in result.exception.__str__()
 
     def test_manage_db_upgrade_unsuccessfully(self):
         """
@@ -96,6 +158,33 @@ class TestCLI(Test):
                     cli, ["manage", "db", "upgrade", "--url", self.url, tmp]
                 )
                 assert "Database main upgraded" in result.output
+
+    def test_manage_db_populate_exception(self):
+        """
+        Given: Current directory is a project directory
+        When: Running 'illuminate manage db populate' referencing wrong
+        database type with selector option
+        Expected: BasicManagerException is raised
+        """
+        with self.path() as path:
+            name = "example"
+            runner = CliRunner()
+            with runner.isolated_filesystem(temp_dir=path) as tmp:
+                runner.invoke(cli, ["manage", "project", "setup", name, "."])
+                runner.invoke(
+                    cli, ["manage", "db", "revision", "--url", self.url, tmp]
+                )
+                runner.invoke(
+                    cli, ["manage", "db", "upgrade", "--url", self.url, tmp]
+                )
+                result = runner.invoke(
+                    cli,
+                    ["manage", "db", "populate", "--selector", "measurements"],
+                )
+                assert (
+                    "Command populate can only be performed on SQL "
+                    "database, measurements is not supported SQL database"
+                ) in result.exception.__str__()
 
     def test_manage_db_populate_unsuccessfully(self):
         """
